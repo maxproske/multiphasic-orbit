6ecdbfb85785a3198dbacb8fbd0897aa7b031774
Max Proske <mhcproske@gmail.com>
2018-01-20 20:04:19 +0000

    Add Basic Panel

---
+++ Assets/Scripts/CloseOnBlur.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseOnBlur : MonoBehaviour {
+
+	// Check whether or not the mouse is hovering over the canvas panel
+	private bool mouseHover;
+
+	public void SetHover (bool hover) {
+		mouseHover = hover;
+	}
+
+	// Close itself when player clicks and it is off the panel
+	void Update () {
+		// Player is finished clicking
+		if (Input.GetMouseButtonUp (0) && !mouseHover) {
+			gameObject.SetActive (false);
+		}
+	}
+}
