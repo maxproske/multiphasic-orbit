2ba3438ed3940a66c6d05b31ecb93e661176bb5a
Max Proske <mhcproske@gmail.com>
2018-01-21 01:33:51 +0000

    Improved Macro Tree Readability

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 // New blueprint for planets that can be created in Unity editor
 [CreateAssetMenu(fileName = "New Planet", menuName = "Macro Skill Tree/Planet")]
-public class Planet : ScriptableObject {
+public class Planet : MonoBehaviour {
 
 	// "name" is already take. Override using "new"
 	new public string name = "New Planet";
