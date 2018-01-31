94ad792d0f10584e8642e892b0610cd6b6346823
Ray Low <raycolow@gmail.com>
2018-01-31 06:03:10 +0000

    Errors should be fixed now.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class gameController : MonoBehaviour
+public class GameController : MonoBehaviour
 {
     public Button nextTurnButton;
-    public Planet[] planets;
+    public List<GameObject> planets;
 
     public Text planetText;
     public Text carbonText;
@@ -14,18 +14,27 @@ public class gameController : MonoBehaviour
     public Text hydrogenText;
 
     private Planet planetScript;
+
+    public bool simulate;
+
+    private Button nextTurn;
     // Use this for initialization
     void Start()
     {
+        nextTurn = GameObject.Find("End Turn Button").GetComponent<Button>();
+        nextTurn.onClick.AddListener(Simulate);
 
+        planets = new List<GameObject>();
+        simulate = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameObject.Find("Planet(Clone)") != null)
+
+        if (GameObject.Find("Carbon(Clone)") != null)
         {
-            planetScript = GameObject.Find("Planet(Clone)").GetComponent<Planet>();
+            planetScript = GameObject.Find("Carbon(Clone)").GetComponent<Planet>();
             //Debug.Log("Planet(Clone) Carbon: " + planetScript.carbon);
             //Debug.Log("Planet(Clone) Nitrogen: " + planetScript.nitrogen);
             //Debug.Log("Planet(Clone) Hydrogen: " + planetScript.hydrogen);
@@ -34,5 +43,10 @@ public class gameController : MonoBehaviour
             nitrogenText.text = planetScript.nitrogen.ToString();
             hydrogenText.text = planetScript.hydrogen.ToString();
         }
+    }
+
+    void Simulate()
+    {
+        simulate = true;
     }
 }
