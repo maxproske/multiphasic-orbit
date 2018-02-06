86a1b6170c342f62a694b64a9a9710291d8b5db8
Ray Low <raycolow@gmail.com>
2018-02-06 08:44:27 +0000

    Adjusted planet selection so it is handled by GameController where the UI updates.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -15,13 +15,14 @@ public class GameController : MonoBehaviour
     public Text hydrogenText;
 
     private Planet planetScript;
+    private GameObject selected; // hold selected GameObject
 
     public bool simulate;
-	private float startTime;
+    private float startTime;
     private Button nextTurn;
-	private EventSystem es;
-	private bool esgo = false;
-	private int i;
+    private EventSystem es;
+    private bool esgo = false; // handles un/locking of UI
+    private int i;
     // Use this for initialization
     void Start()
     {
@@ -29,44 +30,65 @@ public class GameController : MonoBehaviour
         nextTurn.onClick.AddListener(Simulate);
 
         planets = new List<GameObject>();
-		es = EventSystem.current;
+        es = EventSystem.current;
         simulate = false;
     }
 
     // Update is called once per frame
     void Update()
-    {
-
-
-        if (GameObject.Find("Carbon(Clone)") != null)
+    {     
+        // handles un/locking of UI
+        if (esgo == true)
         {
-            planetScript = GameObject.Find("Carbon(Clone)").GetComponent<Planet>();
-            //Debug.Log("Planet(Clone) Carbon: " + planetScript.carbon);
-            //Debug.Log("Planet(Clone) Nitrogen: " + planetScript.nitrogen);
-            //Debug.Log("Planet(Clone) Hydrogen: " + planetScript.hydrogen);
+            i++;
+            if (i > 150)
+            {
+                es.enabled = true;
+                i = 0;
+                esgo = false;
+            }
+        }
+
+        // This code enables player to click any object in the scene
+        // Also updating text is here so moved selecting planets here
+        // https://answers.unity.com/questions/332085/how-do-you-make-an-object-respond-to-a-click-in-c.html
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // https://docs.unity3d.com/ScriptReference/Input-mousePosition.html
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                selected = hit.collider.gameObject; // put GameObject hit by ray in variable
+            }
+        }
+
+        if (planets.Contains(selected))
+        {
+            planetScript = selected.GetComponent<Planet>(); // get Planet script to access attributes
+
+            // update UI
             planetText.text = planetScript.name;
             carbonText.text = planetScript.carbon.ToString();
             nitrogenText.text = planetScript.nitrogen.ToString();
             hydrogenText.text = planetScript.hydrogen.ToString();
         }
-		if (esgo == true) {
-			i++;
-			if(i>150){
-				es.enabled = true;
-				i = 0;
-				esgo = false;
-			}
-		}
-
+        else
+        {
+            // update UI when no planet is selected
+            planetText.text = "No Planet Selected";
+            carbonText.text = 0.ToString();
+            nitrogenText.text = 0.ToString();
+            hydrogenText.text = 0.ToString();
+        }
     }
 
 
     void Simulate()
     {
         simulate = true;
-		startTime = Time.time;
-		es.enabled = false;
-		esgo = true;
+        startTime = Time.time;
+        es.enabled = false;
+        esgo = true;
     }
 
 
