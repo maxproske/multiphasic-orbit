10789f0dbc08dfe3736196afe468e96d20f728f8
Max Proske <mhcproske@gmail.com>
2018-02-06 02:49:27 +0000

    Prepare for changes to tech tree

--- Assets/Scripts/Technology/TechnologySlot.cs
+++ Assets/Scripts/Technology/TechnologySlot.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class TechnologySlot : MonoBehaviour {
+
+	public Transform technologyParent; // Reference to the empty skill tree slots parent
+	public List<Transform> prereqs; // Reference to the technology slots to draw lines to
 
 	// Use this for initialization
 	void Start () {
-		
+		// Set all planet slot buttons as uninteractable
+		var buttons = technologyParent.GetComponentsInChildren<Button>();
+		for (int i = 0; i < buttons.Length; i++)
+		{
+			if (buttons[i].interactable)
+			{
+				buttons[i].interactable = false;
+			}
+		}
 	}
 	
 	// Update is called once per frame
