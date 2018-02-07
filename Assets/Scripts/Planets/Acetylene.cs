f8eac963562def8b09e1e100bfa70e5a8e35d0b8
Ray Low <raycolow@gmail.com>
2018-02-07 10:03:49 +0000

    Can now build tier 3 planets. Planets do not have prerequisites at the moment.

---
+++ Assets/Scripts/Planets/Acetylene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acetylene : Planet {
+
+    public Acetylene() {
+        addCarbon = 6;
+        addNitrogen = 6;
+        addHydrogen = 6;
+        turnsToBuild = 5;
+    }
+}
---
+++ Assets/Scripts/Planets/Germanium.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Germanium : Planet {
+
+    public Germanium() {
+        addCarbon = 0;
+        addNitrogen = 15;
+        addHydrogen = 20;
+        turnsToBuild = 5;
+    }
+}
