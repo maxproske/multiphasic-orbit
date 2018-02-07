0958ce5802b2a830b1bc6979589746007ecf0b8c
Ray Low <raycolow@gmail.com>
2018-02-07 09:46:31 +0000

    Can now build all Tier 2 planets

---
+++ Assets/Scripts/Planets/Ammonia.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammonia : Planet {
+
+    public Ammonia() {
+        addCarbon = 2;
+        addNitrogen = 6;
+        addHydrogen = 2;
+        turnsToBuild = 3;
+    }
+}
---
+++ Assets/Scripts/Planets/Methane.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Methane : Planet {
+
+    public Methane() {
+        addCarbon = 2;
+        addNitrogen = 2;
+        addHydrogen = 6;
+        turnsToBuild = 3;
+    }
+}
