f7be4b054e90d10adf64f26fd48cede17a1e61c8
Ray Low <raycolow@gmail.com>
2018-03-28 07:54:47 +0000

    Cutscene with 2 placeholder images

---
+++ Assets/CutsceneController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutsceneController : MonoBehaviour {
+
+    public Image currentImage;
+    public List<Sprite> intro;
+    public int currentImageNumber;
+
+	// Use this for initialization
+	void Start () {
+        currentImageNumber = 0;
+        currentImage.sprite = intro[currentImageNumber];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("left"))
+        {
+            if (currentImageNumber != 0)
+            {
+                currentImage.canvasRenderer.SetAlpha(0.0f);
+                currentImage.CrossFadeAlpha(1.0f, 1f, false);
+                currentImageNumber--;
+                currentImage.sprite = intro[currentImageNumber];
+            }
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            if (currentImageNumber < intro.Count - 1)
+            {
+                currentImage.canvasRenderer.SetAlpha(0.0f);
+                currentImage.CrossFadeAlpha(1.0f, 1f, false);
+                currentImageNumber++;
+                currentImage.sprite = intro[currentImageNumber];
+            }
+        }
+    }
+}
