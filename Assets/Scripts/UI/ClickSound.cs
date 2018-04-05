49ceee48a773c80582d5a528075afd91a3137579
Max Proske <mhcproske@gmail.com>
2018-04-05 01:43:48 +0000

    Add hover and click SFX to all buttons

--- Assets/Scripts/UI/ClickSound.cs
+++ Assets/Scripts/UI/ClickSound.cs
@@ -1,27 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Button))]
-public class ClickSound : MonoBehaviour 
+public class ClickSound : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public AudioClip sound;
+    private AudioClip clickAudioClip;
+    private AudioClip hoverAudioClip;
     private Button button { get { return GetComponent<Button>(); } }
-    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+    private AudioSource audioSource { get { return GetComponent<AudioSource>(); } }
 
     // Use this for initialization
     void Start () 
     {
+        // Add audio source
         gameObject.AddComponent<AudioSource>();
-        source.clip = sound;
-        source.volume = 0.2f;
-        source.playOnAwake = false;
+
+        // Load resources
+        hoverAudioClip = (AudioClip)Resources.Load<AudioClip>("Audio/menu-fx-02");
+        
+        clickAudioClip = (AudioClip)Resources.Load<AudioClip>("Audio/menu-fx-03");
         button.onClick.AddListener(() => PlaySoud());
+
+        audioSource.volume = 0.4f;
+        audioSource.playOnAwake = false;
     }
 
-    // Update is called once per frame
+	public void OnPointerEnter (PointerEventData eventData)
+	{
+        if (button.IsInteractable())
+        {
+            audioSource.PlayOneShot (hoverAudioClip);
+        }
+	}
+	public void OnPointerExit (PointerEventData eventData)
+	{
+
+	}
+
     void PlaySoud ()
     {
-        source.PlayOneShot (sound);
+        audioSource.PlayOneShot (clickAudioClip);
     }
 }
